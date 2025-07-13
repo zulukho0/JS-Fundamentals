5c1dfe4762d430c57a62870b7c6091353c54c34f
skroof <zulukho0@gmail.com>
2025-07-13 14:14:19 +0200

    add exercise 3

---
+++ 2-arguments.js
@@ -0,0 +1,13 @@
+function argumentsChecker(){
+    let hold = arguments.length
+
+    if (hold == 0){
+        console.log("No argument");
+    }else if (hold == 1) {
+        console.log("Argument found");
+    }else{
+        console.log("Arguments found"); 
+    }
+}
+
+name("test", 30);
